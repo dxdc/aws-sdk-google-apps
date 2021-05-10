ef38b6b9246e1adc51ed1d8a67bea5ca88958c52
Daniel Caspi <dan@element26.net>
2021-05-10 18:43:00 -0500

    Updated setTimeout to support arguments
    Updated setTimeout to support arguments

--- src/Polyfill.js
+++ src/Polyfill.js
@@ -1,4 +1,8 @@
-async function setTimeout(fn, delay) {
-  Utilities.sleep(delay);
-  fn();
+async function setTimeout() {
+  const args = Array.prototype.slice.call(arguments);
+  const handler = args.shift();
+  const ms = args.shift();
+  Utilities.sleep(ms);
+
+  return handler.apply(this, args);
 }
