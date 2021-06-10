b46f3b496c03cdb872d82ea98b61c92fcc2214d1
Daniel Caspi <dan@element26.net>
2021-06-09 22:41:41 -0500

    Update dependencies; AWS-SDK

--- src-sdk/aws-sdk.js
+++ src-sdk/aws-sdk.js
@@ -1,4 +1,4 @@
-// AWS SDK for JavaScript v2.912.0
+// AWS SDK for JavaScript v2.925.0
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // License at https://sdk.amazonaws.com/js/BUNDLE_LICENSE.txt
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
@@ -6403,6 +6403,9 @@ module.exports={
   },
   "apprunner": {
     "name": "AppRunner"
+  },
+  "proton": {
+    "name": "Proton"
   }
 }
 },{}],10:[function(require,module,exports){
@@ -14956,7 +14959,7 @@ AWS.util.update(AWS, {
   /**
    * @constant
    */
-  VERSION: '2.912.0',
+  VERSION: '2.925.0',
 
   /**
    * @api private
@@ -34162,7 +34165,7 @@ var LRUCache = /** @class */ (function () {
 }());
 exports.LRUCache = LRUCache;
 },{}],117:[function(require,module,exports){
-// AWS SDK for JavaScript v2.912.0
+// AWS SDK for JavaScript v2.925.0
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // License at https://sdk.amazonaws.com/js/BUNDLE_LICENSE.txt
 require('./browser_loader');
